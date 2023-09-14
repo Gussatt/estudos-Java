a8e61220a57455022b5300aa94a3fdfbd4653bad
Gustavo Saturnino <ga38185@gmail.com>
2023-09-14 20:17:40 -0300

    delete var-flux-syntax

--- var-flux-syntax/src/factorialEx.java
+++
@@ -1,8 +0,0 @@
-
-public class factorialEx{
-	public static void main(String[] args) {
-		for(int n=1, total= 1; n<=10 ;  n++ , total*=n) {
-			System.out.println("Fatorial de " + n + " = " + total);
-		}
-	}
-}
